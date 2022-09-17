a1fda8988c9168dbce3b2f5bb0f596e27b5afba5
Peter van der Veen <p.vanderveen93@outlook.com>
2022-09-17 09:18:39 +0200

    Add open switch

--- Touch/Program.cs
+++ Touch/Program.cs
@@ -9,16 +9,21 @@ public static class Program
     private static readonly string? AssemblyName = GetExecutingAssembly().GetName().Name;
     private static readonly string? AssemblyVersion = GetExecutingAssembly().GetName().Version?.ToString();
 
+    private static readonly Dictionary<string, Func<Task>> Parameters = new()
+    {
+        {"-o", OpenFile}
+    };
+
     private static void Main(string[] args)
     {
-        _filePath = args.Length > 0 ? args[0] : string.Empty;
-
-        if (string.IsNullOrEmpty(_filePath))
+        var destination = args.FirstOrDefault(x => !x.StartsWith("-"));
+        if (string.IsNullOrEmpty(destination))
         {
             ShowHelp();
             Environment.Exit(0);
         }
 
+        _filePath = destination;
         if (!File.Exists(_filePath))
         {
             CreateFile();
@@ -26,6 +31,8 @@ public static class Program
         }
 
         UpdateTimes();
+        ParseSwitches(args.Where(x => x.StartsWith("-")));
+
         Environment.Exit(0);
     }
 
@@ -62,5 +69,39 @@ public static class Program
         Console.WriteLine("Usage: {0} path", BinaryName);
         Console.WriteLine("{0} textfile.txt", BinaryName);
         Console.WriteLine("{0} \"C:\\Path To\\File\\filename.ext\"", BinaryName);
+    }
+
+    private static void ParseSwitches(IEnumerable<string> switches)
+    {
+        foreach (var arg in switches)
+        {
+            var argument = Parameters.Where(x => x.Key == arg).ToList();
+            if (!argument.Any())
+            {
+                Console.WriteLine($"Unknown switch: {arg}");
+                continue;
+            }
+
+            try
+            {
+                argument.FirstOrDefault().Value.Invoke();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    private static Task OpenFile()
+    {
+        if (!File.Exists(_filePath))
+        {
+            Console.WriteLine($"{_filePath} does not exist");
+        }
+
+        ShellHelper.OpenFile(_filePath);
+        return Task.CompletedTask;
     }
 }
